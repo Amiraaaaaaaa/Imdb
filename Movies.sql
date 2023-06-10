5327bbd7d86cdb54c1a52a6f6e1cbf422388e93d
Amira Mostafa <amiraamgad5@gmail.com>
2023-06-10 23:29:33 +0200

    Add files via upload

---
+++ Movies.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO Movies (Series_Title, Released_Year, Genre, IMDB_Rating, Overview, Star1, Star2, Star3, star4)
+
+
+VALUES 
+
+('Shawshank Redemption', 1994, Drama, 9.3, 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 'Tim Robbins', 'Morgan Freeman','Bob Gunton','William Sadler'),
+('The Godfather',1972,Drama,9.2,'An organized crime dynastys aging patriarch transfers control of his clandestine empire to his reluctant son.','Marlon Brando','Al Pacino','James Caan','Diane'),
+('The Dark Knight', 2008,'Action',9,'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.','Christian Bale','Heath Ledger','(Aaron Eckhart','Michael Caine'),
+('The Godfather: Part II',1974'Crime',9,'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.','Al Pacino','Robert De Niro','Robert Duvall','Diane Keaton'),
+('12 Angry Men',1957,'Adventure',8.9,'A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.','Henry Fonda','Lee J. Cobb','Martin Balsam','John Fiedler')
+
---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS Movies (
+    Series_Title text PRIMARY KEY,
+    Rating float,
+    Released_Year integer,
+    Overview text,
+    Genre varchar (50),
+    Star1 varchar (50),
+    Star2 varchar (50),
+    Star3 varchar (50),
+    star4 varchar (50)
+);
+
